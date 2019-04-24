1cb3c25dbd9e3c2c57dde71cbeaa60b61e4f92bb
wsy1314 <523106472@qq.com>
2019-04-24 11:07:48 +0800

    add cross origin

--- src/main/java/io/wsy/blockchainexplorer/controller/BlockController.java
+++ src/main/java/io/wsy/blockchainexplorer/controller/BlockController.java
@@ -11,10 +11,7 @@ import io.wsy.blockchainexplorer.dto.BlockListDTO;
 import io.wsy.blockchainexplorer.po.Block;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.LinkedList;
@@ -23,6 +20,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/block")
+@CrossOrigin
 public class BlockController {
 
     @Autowired
